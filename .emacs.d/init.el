b72b98c3767e1c9f4775d02968349b233d838880
Andreas Scholta <andreas.scholta@gmail.com>
2015-01-18 17:11:40 +0100

    set helm-split-window-in-side-p to nil

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -212,8 +212,7 @@
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
 
-(setq helm-split-window-in-side-p t ;open helm buffer inside current window, not
-                                    ;occupy whole other window
+(setq helm-split-window-in-side-p nil
       helm-move-to-line-cycle-in-source t ;move to end or beginning of source
                                           ;when reaching top or bottom of
                                           ;source.
